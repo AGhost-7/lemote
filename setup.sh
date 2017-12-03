eaddf373377927b4f6d681e6e73b120d189f8541
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-12-03 15:47:57 -0500

    Fix bugs with setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 git submodule update --init
 sudo apt-get install build-essential -y
 sudo apt-get install flex -y
@@ -7,6 +9,7 @@ sudo apt-get install python3-pip -y
 sudo apt-get install libcwiid-dev -y
 sudo apt-get install python3-dev -y
 sudo apt-get install automake -y
+sudo apt-get install bison -y
 
 pushd cwiid
 aclocal
